6ba547e0b8d13cdefc37056ba5e97ca412deb8aa
vlotar <vadym.lotar@rakuten.de>
2013-06-11 10:34:24 +0200

    Initialize JedisDemo project

---
+++ JedisDemo/src/main/java/com/vadymlotar/demo/JedisDemo/App.java
@@ -0,0 +1,13 @@
+package com.vadymlotar.demo.JedisDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
